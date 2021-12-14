addc0d6371155db9e40ee3ebcc4ea26582f57c3e
BrondoL <nabilunited2@gmail.com>
2021-12-14 13:15:33 +0700

    Add migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string("avatar")->nullable();
+            $table->string("occupation")->nullable();
+            $table->boolean("is_admin")->default(false);
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
