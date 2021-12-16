4f3fc912638726f735fabfa180e6bd4366476949
BrondoL <nabilunited2@gmail.com>
2021-12-16 09:47:00 +0700

    Integration landing page, checkout and dashboard

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Checkout extends Model
 {
@@ -19,4 +20,14 @@ class Checkout extends Model
         'cvc',
         'is_paid',
     ];
+
+    /**
+     * Get the Camps that owns the Checkout
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function Camp(): BelongsTo
+    {
+        return $this->belongsTo(Camp::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,20 +20,16 @@ Route::get('/', function () {
     return view('welcome');
 })->name("home");
 
-Route::get('/checkout', function () {
-    return view('checkout');
-})->name("checkout");
+Route::middleware(['auth'])->group(function () {
+    Route::get('checkout/success', [CheckoutController::class, 'success'])->name('checkout.success');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class, 'create'])->name('checkout.create');
+    Route::post('checkout/{camp}', [CheckoutController::class, 'store'])->name('checkout.store');
 
-Route::get('/success-checkout', function () {
-    return view('success-checkout');
-})->name("success-checkout");
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->name("dashboard");
+    Route::get('dashboard', [HomeController::class, 'dashboard'])->name('dashboard');
+});
 
 // socialite
-Route::get('/sign-in-google', [UserController::class, 'google'])->name("user.login.google");
-Route::get('/auth/google/callback', [UserController::class, 'handleProviderCallback'])->name("user.google.callback");
+Route::get('sign-in-google', [UserController::class, 'google'])->name("user.login.google");
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name("user.google.callback");
 
 require __DIR__ . '/auth.php';
