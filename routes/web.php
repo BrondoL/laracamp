176d1c09096f17f72cfe312348c4cf0decb922c5
BrondoL <nabilunited2@gmail.com>
2021-12-19 22:06:17 +0700

    Add Set to Paid Checkout

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\User\DashboardController as UserDashboard;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
+use App\Http\Controllers\Admin\CheckoutController as AdminCheckout;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,6 +36,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->middleware("user.role:admin")->group(function (){
         Route::get('/', [AdminDashboard::class, 'index'])->name('dashboard');
+        Route::post('checkout/{checkout}', [AdminCheckout::class, 'update'])->name('checkout.update');
     });
 });
 
